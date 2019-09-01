8e06e7289dbf80fec23fb25cf188367290ccd7e7
Rx9711 <1763068382@qq.com>
2019-09-01 10:36:55 +0800

    版本1.0

--- src/xyz/qixuanliu/test/TestProgram.java
+++ src/xyz/qixuanliu/test/TestProgram.java
@@ -4,6 +4,11 @@ import xyz.qixuanliu.excel.SaveData2DB;
 
 public class TestProgram {
 
+    /**
+     * 测试功能
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args)throws Exception{
         SaveData2DB.save("C:\\Users\\dell-1\\Desktop\\Temp\\student_info.xls","xyz.qixuanliu.entity.Student");
         //ReadDateToExcel.read("xyz.qixuanliu.entity.Student", "D://student.xls");
